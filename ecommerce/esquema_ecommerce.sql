971faaed8609073f37d15ce035f131f86c16c098
Fernando Schmitz <fdtschmitz.dev@gmail.com>
2023-12-13 05:56:31 -0300

    Primeiro commit

---
+++ ecommerce/esquema_ecommerce.sql
@@ -0,0 +1,17 @@
+-- criação do banco de dados para e-commerce
+create database ecommerce;
+use ecommerce;
+
+-- criação da da tabela cliente
+
+
+-- criação da da tabela cliente
+
+
+-- criação da da tabela cliente
+
+
+-- criação da da tabela cliente
+
+
+-- criação da da tabela cliente
